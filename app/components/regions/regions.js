43de701335c246e699edc7191f33ffd410d08c6e
Aaron Goddard <genxhero@users.noreply.github.com>
2020-01-05 11:35:18 -0800

    start the constants creation

---
+++ app/components/regions/regions.js
@@ -0,0 +1,14 @@
+const Regions = [
+    {
+        name: "Crescent Coast",
+        overview: "A heavily settled region surrounding a bay (or a sea depending on whom you ask)roughly the shape of a crescent moon.  Legend has it that the Dragon Empress used her significant power to flood the lands surrounding her castle in order to create the ultimate moat full of horrors from the depths.  Over the centuries the number of monsters in the bay has decreased, but will most likely never be depleted.",
+        nations: [
+            {name: "Gaille",
+            places: [
+                {name: "Loupblanc", description: "Capitol of Gaille, offcially, though the coastal city of Parseilles is more influential.  The city is shrouded in mystery, with fog-choked streets, haunting architecture, and sightings of terrible creatures that are more than just rumors."},
+                {name: "Parseilles", description: "Cultural and trade center of Gaille. It was built on and around the mouth of the White River.  The Cascading Palace is an architectual marvel to behold."}
+            ] 
+        }
+        ]
+    }
+]
