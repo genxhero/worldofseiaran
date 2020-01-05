b3044c360529aa87083453475a1df051c48dcdd5
Aaron Goddard <genxhero@users.noreply.github.com>
2020-01-05 11:39:56 -0800

    documentation for regions constant

--- app/components/regions/regions.js
+++ app/components/regions/regions.js
@@ -1,5 +1,17 @@
-const Regions = [
-    {
+/**
+ * Procedure
+ * 
+ * Pass the object that is stored at a region's key into the region component.
+ * Then load all of the data therein.
+ * TODO: Do this for all regions in the campaign setting.
+ * 
+ * JUSTIFICATION: I want to use github pages so  i can't have a backend.
+ * Normally I'd use a relational database of some kind. Heck mayne for fun I'll
+ * build something in Rails later that does the same thing.
+ */
+
+const REGIONS = {
+    "crescentcoast": {
         name: "Crescent Coast",
         overview: "A heavily settled region surrounding a bay (or a sea depending on whom you ask)roughly the shape of a crescent moon.  Legend has it that the Dragon Empress used her significant power to flood the lands surrounding her castle in order to create the ultimate moat full of horrors from the depths.  Over the centuries the number of monsters in the bay has decreased, but will most likely never be depleted.",
         nations: [
@@ -11,4 +23,6 @@ const Regions = [
         }
         ]
     }
-]
+}
+
+export default REGIONS;
