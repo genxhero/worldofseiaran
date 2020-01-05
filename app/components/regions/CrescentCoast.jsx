0c01a1eb769053d5c4692b0fed4c5937b9cad6c5
Aaron Goddard <genxhero@users.noreply.github.com>
2020-01-05 11:00:53 -0800

    butt

--- app/components/regions/CrescentCoast.jsx
+++ app/components/regions/CrescentCoast.jsx
@@ -3,9 +3,11 @@ import {withRouter} from 'react-router';
 
 
 const CrescentCoast = () => {
-    return <div>
-        The Crescent Coast.
+    return( 
+    <div className="region-detail-page">
+        <h1 className="region-title">The Crescent Coast</h1>
     </div>
+    )
 }
 
 export default withRouter(CrescentCoast);
