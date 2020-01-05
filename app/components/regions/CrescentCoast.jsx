43dcbc2ea433a9f0f68df9740d38ec7bb9370e1f
Aaron Goddard <genxhero@users.noreply.github.com>
2020-01-05 11:27:29 -0800

    wrap some things in a div

--- app/components/regions/CrescentCoast.jsx
+++ app/components/regions/CrescentCoast.jsx
@@ -12,17 +12,27 @@ const CrescentCoast = () => {
             (some call it a sea) and the surrounding lands.
         </p>
         <h2 className="region-section-title"> Nations</h2>
-        <h3 className="region-subsection-title"> Gaille</h3>
-        <p className="region-nation-overview"> 
-            The most prosperous kingdom on the Crescent Coast, it was founded in the early days of the Fourth Age by heroes tasked with mopping up what was left of The Dragon Empress' forces.  The symbol of Gaille is a winged rapier over a sky blue background.  
-        </p>
-        <h4 className="region-nation-subsection-title">Places</h4>
-        <p className="region-nation-subsection-body"> <span className="bold">Loupblanc:</span> Capitol of Gaille, offcially, though the coastal city of Parseilles is more influential.  The city is shrouded in mystery, with fog-choked streets, haunting architecture, and sightings of terrible creatures that are more than just rumors.</p>
-        <p className="region-nation-subsection-body"> <span className="bold"> Parseilles:</span>  Cultural and trade center of Gaille. It was built on and around the mouth of the White River.  The Cascading Palace is an architectual marvel to behold.  </p>
+        <div className="region-nation">
+            <h3 className="region-subsection-title"> Gaille</h3>
+            <p className="region-nation-overview"> 
+                The most prosperous kingdom on the Crescent Coast, it was founded in the early days of the Fourth Age by heroes tasked with mopping up what was left of The Dragon Empress' forces.  The symbol of Gaille is a winged rapier over a sky blue background.  
+            </p>
+            <div className="region-nation-subsection">
+                <h4 className="region-nation-subsection-title">Places</h4>
+                <p className="region-nation-subsection-body"> <span className="bold">Loupblanc:</span> Capitol of Gaille, offcially, though the coastal city of Parseilles is more influential.  The city is shrouded in mystery, with fog-choked streets, haunting architecture, and sightings of terrible creatures that are more than just rumors.</p>
+                <p className="region-nation-subsection-body"> <span className="bold"> Parseilles:</span>  Cultural and trade center of Gaille. It was built on and around the mouth of the White River.  The Cascading Palace is an architectual marvel to behold.  </p>
+            </div>
 
-        <h4 className="region-nation-subsection-title">People</h4>
-        <p className="region-nation-subsection-body"></p>
+            <div className="region-nation-subsection">
+                 <h4 className="region-nation-subsection-title">People</h4>
+                 <p className="region-nation-subsection-body"><span className="bold">Gabrielle Carmanet</span></p>
+                 <p className="region-nation-subsection-body"><span className="bold">Poppy Greenbottle</span></p>
+                 <p className="region-nation-subsection-body"><span className="bold">Guillarme</span></p>
 
+            </div>
+            
+        </div>
+        
         <h3 className="region-subsection-title"> Dukedom of St. Tevas</h3>
         <p className="region-nation-overview"> </p>
         <h4 className="region-nation-subsection-title">Places</h4>
